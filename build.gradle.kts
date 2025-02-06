8201e6c39a477ce2450202a01b4c099f1f6b1010
andrepagans <andreapagani26@gmail.com>
2025-02-06 19:22:50 +0100

    fix: added explicit dependencies in the tasks block

--- build.gradle.kts
+++ build.gradle.kts
@@ -136,16 +136,42 @@ tasks {
     bootJar {
         enabled = true
     }
+    
     jar {
         enabled = true
     }
+    
     shadowJar {
         archiveClassifier.set("") // Rimuove il suffisso "-all"
         mergeServiceFiles()
-        configurations = listOf(project.configurations.runtimeClasspath.get()) // Includi tutte le dipendenze
+        configurations = listOf(project.configurations.runtimeClasspath.get())
         manifest {
             attributes["Main-Class"] = "it.unibo.samplejavafx.App"
         }
+    }
+    
+    // Aggiungi le dipendenze esplicite
+    bootDistZip {
+        dependsOn(shadowJar)
+    }
+    
+    bootDistTar {
+        dependsOn(shadowJar)
+    }
+    
+    bootStartScripts {
+        dependsOn(shadowJar)
+    }
+    
+    // Aggiungi la dipendenza per startShadowScripts
+    startShadowScripts {
+        dependsOn(bootJar)
+    }
+    
+    // Assicurati che tutti i task necessari vengano eseguiti nell'ordine corretto
+    build {
+        dependsOn(shadowJar)
+        dependsOn(bootJar)
     }
 }
 
